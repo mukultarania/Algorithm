ca740dfb45196e0fe26227b7aa9879e49d853fec
Mukul Tarania <43871224+mukultarania@users.noreply.github.com>
2020-04-12 21:01:14 +0530

    Double Linked List
    Reverse print, InsertatHead

--- LinkList/doublelink.cpp
+++ LinkList/doublelink.cpp
@@ -7,19 +7,23 @@ struct Node{
     Node* next;
 };
 struct Node* head = NULL;
-void insert(int data){
+struct Node* getNewNode(int x){
     Node* temp = new Node();
-    Node* temp2 = head;
-    temp->data = data;
+    temp->data = x;
+    temp->pre = NULL;
+    temp->next = NULL;
+    return temp;
+}
+void insert(int data){
+    Node* temp = getNewNode(data);
     if(head == NULL){
-        temp->next = NULL;
-        temp->pre = head;
-    } else {
-        temp->next = temp2;
-        temp2->pre = temp;
+        head = temp;
+        return;
     }
+    head->pre = temp;
+    temp->next = head;
     head = temp;
-    display();
+    
 }
 void display(){
    Node* temp = head;
@@ -30,6 +34,19 @@ void display(){
    }
    cout<<"\n";
 }
+
+void revDisplay(){
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    cout<<"Reverse Linked List is\t";
+    while(temp != NULL){
+       cout<<temp->data<<"\t";
+       temp = temp->pre;
+    }
+   cout<<"\n";
+}
 int main(){
     insert(1);
     insert(2);
@@ -37,5 +54,7 @@ int main(){
     insert(4);
     insert(5);
     insert(6);
+    display();
+    revDisplay();
     
 }
